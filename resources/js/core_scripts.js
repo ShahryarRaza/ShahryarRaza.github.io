1fb017cb1c49be744e1a25f45f7d226f0665aa01
Shahryar Raza <13886711+ShahryarRaza@users.noreply.github.com>
2021-06-28 18:39:20 +0530

    Update core_scripts.js

--- resources/js/core_scripts.js
+++ resources/js/core_scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
+
+    var nav_data = $(".nav_header").data("value"); // Check for page. i.e.: home,mynetworks,personalinfo
+    
+    if(nav_data == "home"){ // Triggers only for home page
+
+        // Getting current dateTime and getting hour from that and setting wishes
+        date = new Date();
+        currHours = date.getHours();
+        var greet = "";
+        if(currHours < 12){
+            greet = "Good morning,";
+        }
+        else if(currHours < 18){
+            greet = "Good afternoon,";
+        }
+        else{
+            greet = "Good evening,";
+        }
+
+        $("#day-wish").html(greet); // Setting wishes
+
+        // Close btn behaviour welcome pad
+        $("#close-wlcm-bnr").click(() => {
+            $(".welcome-container").css("display", "none");
+        });
+    }
      
 });
